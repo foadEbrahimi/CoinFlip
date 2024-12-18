d5973cc000c9a587e47dc585914c69a03afb4efb
Foad <fwad40901@gmail.com>
2024-12-18 21:04:22 +0330

    fix ts to js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 import img from './assets/images/circles.svg';
+import { Button } from './components/ui/button';
 export default function App() {
-  return <div>CoinFlip</div>;
+  return (
+    <div>
+      CoinFlip
+      <Button>Click me</Button>
+    </div>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react';
-import { Provider } from './components/ui/provider.jsx';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <App />
   </StrictMode>
 );
