9cf25cdb996184f2dd8198143b0c42d13afd2d59
Foad <fwad40901@gmail.com>
2024-12-19 12:40:49 +0330

    write navList constants

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const Navlist = [
+  {
+    id: 1,
+    label: 'Buy / Sell',
+  },
+  {
+    id: 2,
+    label: 'Grow',
+  },
+  {
+    id: 3,
+    label: 'Markets',
+  },
+  {
+    id: 4,
+    label: 'Business',
+  },
+  {
+    id: 5,
+    label: 'Support',
+  },
+];
