1900333da5657a6daf489ff64d369992f81bb181
Foad <fwad40901@gmail.com>
2024-12-21 13:26:46 +0330

    place svgs left Hero side

--- src/pages/landing/Hero.jsx
+++ src/pages/landing/Hero.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import img from '/assets/images/hero/gradient1.svg';
-import img2 from '/assets/images/hero/gradient2.svg';
-import Navbar from '@/components/myComponents/Hero/Navbar';
-import Content from '@/components/myComponents/Hero/Content';
+import img from "/assets/images/hero/gradient1.svg";
+import img2 from "/assets/images/hero/gradient2.svg";
+import bitcoin from "/assets/images/hero/Bitcoin.svg";
+import LiteCoin from "/assets/images/hero/LiteCoin.svg";
+import star1 from "/assets/images/hero/star.svg";
+import sphere from "/assets/images/hero/sphere.svg";
+import sphere2 from "/assets/images/hero/sphere2.svg";
+
+import Navbar from "@/components/myComponents/Hero/Navbar";
+import Content from "@/components/myComponents/Hero/Content";
 
 export default function Hero() {
   return (
-    <div id="heroBg" className="relative grid grid-rows-12 overflow-hidden z-10 ~px-5/24">
+    <div
+      id="heroBg"
+      className="relative z-10 grid grid-rows-12 overflow-hidden ~px-5/24"
+    >
       <Navbar />
       {/* gradient svgs */}
       <div>
@@ -15,7 +24,7 @@ export default function Hero() {
           src={img}
           alt="gradient svg one"
           draggable={false}
-          className="absolute top-0 left-0 -z-10"
+          className="absolute left-0 top-0 -z-10"
         />
         <img
           src={img2}
@@ -25,7 +34,38 @@ export default function Hero() {
         />
       </div>
       {/* end */}
-      <Content/>
+      <Content />
+      {/* hanging stars */}
+      <div>
+        <div className="relative">
+          <img
+            draggable={false}
+            className="absolute -top-10"
+            src={bitcoin}
+            alt="bitcoin svg"
+          />
+          <img
+            draggable={false}
+            className="absolute left-36 top-32"
+            src={star1}
+            alt="bitcoin svg"
+          />
+          <img
+            draggable={false}
+            className="absolute top-64"
+            src={sphere2}
+            alt="bitcoin svg"
+          />
+          <img
+            draggable={false}
+            className="absolute top-[29rem] left-36"
+            src={LiteCoin}
+            alt="bitcoin svg"
+          />
+        </div>
+        <div></div>
+      </div>
+      {/* end */}
     </div>
   );
 }
