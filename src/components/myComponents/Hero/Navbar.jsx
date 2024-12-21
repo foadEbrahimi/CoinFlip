fa6930eb3b097fef6ea1081f121fe2e7116b2749
Foad <fwad40901@gmail.com>
2024-12-21 12:55:01 +0330

    fix logo mode-theme

--- src/components/myComponents/Hero/Navbar.jsx
+++ src/components/myComponents/Hero/Navbar.jsx
@@ -1,28 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import { Button } from '../../ui/button';
-import { ModeToggle } from '../../ui/mode-toggle';
+import { Button } from "../../ui/button";
+import { ModeToggle } from "../../ui/mode-toggle";
 
-import { Navlist } from '../../../constants/index';
-import { useTheme } from '../../ui/theme-provider';
+import { Navlist } from "../../../constants/index";
+import { useTheme } from "../../ui/theme-provider";
 
 export default function Navbar() {
   const { theme } = useTheme();
   const logoSrc =
-    theme === 'dark'
-      ? './assets/images/logo/Logo.svg'
-      : './assets/images/logo/Logo-dark.svg';
+    theme === "light"
+      ? "./assets/images/logo/Logo-dark.svg"
+      : "./assets/images/logo/Logo.svg";
 
   return (
-    <div className='row-span-1'>
+    <div className="row-span-1">
       <div className="flex items-center justify-between py-5">
         <div>
           <img src={logoSrc} alt="Logo CoinFlip" />
         </div>
-        <div>
+        <div className="hidden md:flex">
           <ul className="flex items-center gap-3">
-            {Navlist.map(item => (
-              <li key={item.id} className="cursor-pointer transition-all duration-300 hover:bg-navItemHover p-1 px-3 rounded-md">
+            {Navlist.map((item) => (
+              <li
+                key={item.id}
+                className="cursor-pointer rounded-md p-1 px-3 transition-all duration-300 hover:bg-navItemHover"
+              >
                 {item.label}
               </li>
             ))}
@@ -30,7 +33,9 @@ export default function Navbar() {
         </div>
         <div className="flex items-center gap-3">
           <ModeToggle />
-          <Button className="bg-gradient-to-l from-[#18C8FF] to-[#933FFE] dark:from-[#933FFE] dark:to-[#18C8FF] text-foreground">Sign up</Button>
+          <Button className="bg-gradient-to-l from-[#18C8FF] to-[#933FFE] text-foreground dark:from-[#933FFE] dark:to-[#18C8FF]">
+            Sign up
+          </Button>
         </div>
       </div>
     </div>
