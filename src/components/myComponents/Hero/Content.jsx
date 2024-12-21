6e93d26a7508fdb611115d63bc0a145eae6c9043
Foad <fwad40901@gmail.com>
2024-12-21 13:21:11 +0330

    add svg Content

--- src/components/myComponents/Hero/Content.jsx
+++ src/components/myComponents/Hero/Content.jsx
@@ -1,13 +1,21 @@
-import { Button } from "@/components/ui/button";
 import React from "react";
+
+import { Button } from "@/components/ui/button";
+
+import glass from "/assets/images/hero/glass.svg";
 
 export default function Content() {
   return (
     <div className="row-span-10 -row-start-9">
-      <h1 className="mx-auto text-center font-Bold !leading-[120%] ~text-5xl/7xl lg:~max-w-2xl/xl">
+      <h1 className="relative mx-auto text-center font-Bold !leading-[130%] ~text-5xl/7xl lg:~max-w-2xl/xl">
         We make crypto clear and simple
+        <img
+          src={glass}
+          alt="glass svg"
+          className="absolute -bottom-7 left-0 -z-10 ~w-28/32"
+        />
       </h1>
-      <p className="mx-auto mt-3 max-w-xl text-center font-Regular md:text-lg text-[#898CA9]">
+      <p className="mx-auto mt-3 max-w-xl text-center font-Regular text-[#898CA9] md:text-lg">
         Buy, sell, and grow your crypto with CoinFlip, the platform dedicated
         to every trader at every level.
       </p>
