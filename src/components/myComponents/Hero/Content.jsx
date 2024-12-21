73bf8448dcb3f13a1c56d7b0a0f4ea63a7d2628b
Foad <fwad40901@gmail.com>
2024-12-21 12:55:23 +0330

    responsive Content Hero

--- src/components/myComponents/Hero/Content.jsx
+++ src/components/myComponents/Hero/Content.jsx
@@ -1,15 +1,24 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 export default function Content() {
   return (
-    <div className='row-span-10 -row-start-9'>
-      <h1 className='font-Bold text-7xl text-center max-w-xl leading-[130%] mx-auto'>We make crypto clear and simple</h1>
-
-      <p className='text-[#898CA9] font-Regular text-center mx-auto max-w-lg mt-3'>Buy, sell, and grow your crypto with CoinFlip, the platform dedicated to every trader at every level.</p>
-      <div className='flex items-center justify-center mt-5'>
-        <Button size="lg" className="bg-gradient-to-l from-[#18C8FF] to-[#933FFE] dark:from-[#933FFE] dark:to-[#18C8FF] text-foreground" >Get Started</Button>
+    <div className="row-span-10 -row-start-9">
+      <h1 className="mx-auto text-center font-Bold !leading-[120%] ~text-5xl/7xl lg:~max-w-2xl/xl">
+        We make crypto clear and simple
+      </h1>
+      <p className="mx-auto mt-3 max-w-xl text-center font-Regular md:text-lg text-[#898CA9]">
+        Buy, sell, and grow your crypto with CoinFlip, the platform dedicated
+        to every trader at every level.
+      </p>
+      <div className="mt-5 flex items-center justify-center">
+        <Button
+          size="lg"
+          className="bg-gradient-to-l from-[#18C8FF] to-[#933FFE] text-foreground dark:from-[#933FFE] dark:to-[#18C8FF]"
+        >
+          Get Started
+        </Button>
       </div>
     </div>
-  )
+  );
 }
