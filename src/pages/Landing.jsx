10f1f8474085d6ee39169ef4416784bf16c9ab97
Foad <fwad40901@gmail.com>
2024-12-23 20:37:48 +0330

    Landing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,13 +2,16 @@ import React from "react";
 
 import Hero from "./landing/Hero";
 import CardMarket from "@/components/myComponents/MarketTrend/Card";
+import { MarketTrending } from "@/constants";
 
 export default function Landing() {
   return (
     <div className="">
       <Hero />
-      <div className="~px-5/24 py-10">
-        <CardMarket />
+      <div className="grid -mt-20 grid-cols-4 py-10 ~px-5/24">
+        {MarketTrending.map((item) => (
+          <CardMarket {...item} key={item.id} />
+        ))}
       </div>
     </div>
   );
