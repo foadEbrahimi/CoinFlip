192e2e739088b2cee8cb575ba8194c0952276fab
Foad <fwad40901@gmail.com>
2024-12-24 16:55:42 +0330

    write Faetures constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,6 +3,11 @@ import etherium from "/assets/images/MarketTrend/etherium.svg";
 import bnb from "/assets/images/MarketTrend/bnb.svg";
 import usdt from "/assets/images/MarketTrend/usdt.svg";
 
+import faetures1 from "/assets/images/Faetures/feature-image1.svg";
+import faetures2 from "/assets/images/Faetures/feature-image2.svg";
+import faetures3 from "/assets/images/Faetures/feature-image3.svg";
+import faetures4 from "/assets/images/Faetures/feature-image4.svg";
+
 export const Navlist = [
   {
     id: 1,
@@ -57,5 +62,31 @@ export const MarketTrending = [
     badge: "TETHER",
     price: "0.9998",
     percentage: "0,03",
+  },
+];
+export const FaeturesInfo = [
+  {
+    id: 1,
+    img: faetures1,
+    title: "Manage Portfolio",
+    decs: "Buy and sell popular digital currencies, keep track of them in the one place.",
+  },
+  {
+    id: 2,
+    img: faetures2,
+    title: "Protected Securely",
+    decs: "All cash balances are covered by FDIC insurance, up to a maximum of $250,000.",
+  },
+  {
+    id: 3,
+    img: faetures3,
+    title: "Cryptocurrency Variety",
+    decs: "Supports a variety of the most popular digital currencies and always uptodate.",
+  },
+  {
+    id: 4,
+    img: faetures4,
+    title: "Learn Best Practice",
+    decs: "Easy to know how to cryptocurrency works and friendly to newbie.",
   },
 ];
