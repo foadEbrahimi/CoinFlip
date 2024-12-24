8e13f96d46ebdf3a28b28198640319dfe87ad2d7
Foad <fwad40901@gmail.com>
2024-12-24 16:56:12 +0330

    add Faetures to landing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Hero from "./landing/Hero";
 import MarketTrend from "./landing/MarketTrend";
+import Faetures from "./landing/Faetures";
 
 export default function Landing() {
   return (
     <div className="">
       <Hero />
       <MarketTrend />
+      <Faetures />
     </div>
   );
 }
