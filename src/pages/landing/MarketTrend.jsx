580ee6daca125351c2076c4bb0a68ffcd9c4f9c0
Foad <fwad40901@gmail.com>
2024-12-24 16:21:47 +0330

    fix MarketTrend

--- src/pages/landing/MarketTrend.jsx
+++ src/pages/landing/MarketTrend.jsx
@@ -6,7 +6,7 @@ import CardMarket from "@/components/myComponents/MarketTrend/Card";
 
 export default function MarketTrend() {
   return (
-    <div className="space-y-3 ~px-5/24">
+    <div className="space-y-5 ~px-5/24">
       <span className="font-SemiBold text-3xl">Market Trend</span>
       <div className="flex flex-wrap gap-5">
         {MarketTrending.map((item) => (
