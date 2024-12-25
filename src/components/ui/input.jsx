0d32c80f9f47791cd8e37f553a07c1fdbab687e2
Foad <fwad40901@gmail.com>
2024-12-25 08:10:05 +0330

    change input ui shadcn

---
+++ src/components/ui/input.jsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+const Input = React.forwardRef(
+  ({ className, children, type, ...props }, ref) => {
+    return (
+      <div className="flex h-10 w-full max-w-[17.5rem] items-center rounded-md border border-[rgba(255,255,255,0.1)] bg-[#160C24] px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[rgba(255,255,255,0.2)] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm">
+        {children}
+        <input
+          type={type}
+          className={cn(
+            "flex h-full w-full rounded-md bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+      </div>
+    );
+  },
+);
+Input.displayName = "Input";
+
+export { Input };
