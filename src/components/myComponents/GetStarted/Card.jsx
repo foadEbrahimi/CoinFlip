de8b80c1f5b30b4c0b8c4997700b3e744feeef00
Foad <fwad40901@gmail.com>
2025-01-01 01:16:34 +0330

    create CardGetStarted

---
+++ src/components/myComponents/GetStarted/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Card, CardContent } from "@/components/ui/card";
+
+export default function CardGetStarted({ label, desc, img }) {
+  return (
+    <Card className="min-w-fit rounded-lg border border-[rgba(255,255,255,0.05)] bg-[rgba(255,255,255,0.02)]">
+      <CardContent className="flex items-center gap-5">
+        <img src={img} alt="svg" />
+        <div className="flex flex-col gap-2">
+          <span className="font-SemiBold text-lg">{label}</span>
+          <p className="font-RegularRoboto text-[#B6B6B6]">{desc}</p>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
