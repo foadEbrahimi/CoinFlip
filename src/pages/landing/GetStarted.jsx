9ea912ec313dc5b37ff882dd56b85435e3070e07
Foad <fwad40901@gmail.com>
2025-01-01 14:54:22 +0330

    GetStarted

--- src/pages/landing/GetStarted.jsx
+++ src/pages/landing/GetStarted.jsx
@@ -9,7 +9,7 @@ import { GetStartedInfo } from "@/constants";
 export default function GetStarted() {
   return (
     <div className="mt-44 flex h-[80dvh] w-full items-center ~px-5/24">
-      <div className="flex w-full flex-col items-start justify-between gap-10 sm:flex-row">
+      <div className="flex w-full flex-col items-start justify-between gap-10 md:flex-row">
         <div className="space-y-6">
           <span className="font-Bold ~text-2xl/4xl">How To Get Started</span>
           <p className="max-w-[23rem] font-RegularRoboto text-lg text-[#B6B6B6]">
