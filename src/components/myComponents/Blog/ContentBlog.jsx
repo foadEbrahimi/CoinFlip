2756bb555d3c6a250dbed5e0e218756ca633f38e
Foad <fwad40901@gmail.com>
2025-01-01 19:46:24 +0330

    ContentBlog

---
+++ src/components/myComponents/Blog/ContentBlog.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ContentBlog() {
+  return (
+    <div className="flex flex-col items-center gap-5">
+      <h2 className="font-Bold ~text-2xl/4xl">Learn About Cryptocurrency</h2>
+      <span className="font-RegularRoboto text-[#B6B6B6]">
+        Learn all about cryptocurrency to start investing
+      </span>
+    </div>
+  );
+}
