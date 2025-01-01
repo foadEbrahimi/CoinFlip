6cee2d9d072fa3a9f1e26200785dd551c956b8ca
Foad <fwad40901@gmail.com>
2025-01-02 00:11:32 +0330

    CardBlog

--- src/components/myComponents/Blog/Card.jsx
+++ src/components/myComponents/Blog/Card.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 
 import { Badge } from "@/components/ui/badge";
+import { CardTitle } from "@/components/ui/card";
 
-export default function Card({ label, className, desc, img, tag }) {
+export default function CardBlog({ label, className, desc, img, tag }) {
   return (
-    <div className={className}>
-      <img src={img} alt="img" className="w-full object-cover" />
-      <div className="flex flex-col gap-4 rounded-b-xl border border-[rgba(255,255,255,0.05)] bg-[rgba(255,255,255,0.02)] p-4 pb-9">
+    <div className={`${className} flex min-h-[391px] w-full flex-col p-0`}>
+      <img src={img} alt="img" className="flex rounded-t-lg w-full flex-1 object-cover" />
+      <div className="flex flex-1 flex-col gap-4 rounded-b-xl border border-[rgba(255,255,255,0.05)] bg-[rgba(255,255,255,0.02)] p-4 pb-9">
         <Badge className="w-fit bg-[rgba(255,255,255,0.07)] px-2 text-[#B6B6B6]">
           {tag}
         </Badge>
-        <span className="font-SemiBold">{label}</span>
-        <p className="text-sm text-[#B6B6B6]">{desc}</p>
+        <CardTitle className="font-SemiBold text-[17px]">{label}</CardTitle>
+        <p className="line-clamp-2 text-sm text-[#B6B6B6]">{desc}</p>
       </div>
     </div>
   );
