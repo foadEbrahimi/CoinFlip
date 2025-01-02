fe0c75add4f1d4aa67faa59f568d72a7edec0108
Foad <fwad40901@gmail.com>
2025-01-02 11:32:25 +0330

    add Footer to Landing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,16 +6,18 @@ import Faetures from "./landing/Faetures";
 import MarketUpdate from "./landing/MarketUpdate";
 import GetStarted from "./landing/GetStarted";
 import Blog from "./landing/Blog";
+import Footer from "./landing/Footer";
 
 export default function Landing() {
   return (
-    <div className="">
+    <div>
       <Hero />
       <MarketTrend />
       <Faetures />
       <MarketUpdate />
       <GetStarted />
       <Blog />
+      <Footer />
     </div>
   );
 }
