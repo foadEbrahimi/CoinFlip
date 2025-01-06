5f71f42e5117ef351a0e7619616633127cafaed0
Foad <fwad40901@gmail.com>
2025-01-06 15:51:56 +0330

    add animation to Content Hero

--- src/components/myComponents/Hero/Content.jsx
+++ src/components/myComponents/Hero/Content.jsx
@@ -31,18 +31,47 @@ export default function Content() {
           className="absolute -bottom-7 left-0 -z-10 ~w-28/32"
         />
       </motion.h1>
-      <motion.p className="mx-auto mt-3 max-w-xl text-center font-Regular text-[#898CA9] md:text-lg">
+      <motion.p
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+        transition={{
+          delay: 1,
+          duration: 2,
+        }}
+        className="mx-auto mt-3 max-w-xl text-center font-Regular text-[#898CA9] md:text-lg"
+      >
         Buy, sell, and grow your crypto with CoinFlip, the platform dedicated
         to every trader at every level.
       </motion.p>
-      <div className="mt-5 flex items-center justify-center">
+      <motion.div
+        initial={{
+          scale: 0,
+          opacity: 0,
+          rotate: 0,
+        }}
+        animate={{
+          scale: 1,
+          opacity: 1,
+          rotate: "360deg",
+        }}
+        transition={{
+          duration: 1,
+          delay: 0.5,
+          ease: "easeOut",
+        }}
+        className="mt-5 flex items-center justify-center"
+      >
         <Button
           size="lg"
           className="bg-gradient-to-l from-[#18C8FF] to-[#933FFE] text-foreground dark:from-[#933FFE] dark:to-[#18C8FF]"
         >
           Get Started
         </Button>
-      </div>
+      </motion.div>
     </div>
   );
 }
