f4c77b3cca5f5b3822a316afbac0d304de2789d7
Foad <fwad40901@gmail.com>
2025-01-16 11:36:49 +0330

    add animation to GetStarted

--- src/components/myComponents/GetStarted/Card.jsx
+++ src/components/myComponents/GetStarted/Card.jsx
@@ -2,16 +2,33 @@ import React from "react";
 
 import { Card, CardContent } from "@/components/ui/card";
 
-export default function CardGetStarted({ label, desc, img }) {
+import { motion } from "motion/react";
+
+export default function CardGetStarted({ label, desc, img, index }) {
   return (
-    <Card className="rounded-lg border border-[rgba(255,255,255,0.05)] bg-[rgba(255,255,255,0.02)]">
-      <CardContent className="flex items-center gap-5">
-        <img src={img} alt="svg" />
-        <div className="flex flex-col gap-2">
-          <span className="font-SemiBold text-lg">{label}</span>
-          <p className="font-RegularRoboto text-[#B6B6B6]">{desc}</p>
-        </div>
-      </CardContent>
-    </Card>
+    <motion.div
+      initial={{
+        x: 100,
+        opacity: 0,
+      }}
+      whileInView={{
+        x: 0,
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+        delay: 0.5 * index,
+      }}
+    >
+      <Card className="rounded-lg border border-[rgba(255,255,255,0.05)] bg-[rgba(255,255,255,0.02)]">
+        <CardContent className="flex items-center gap-5">
+          <img src={img} alt="svg" />
+          <div className="flex flex-col gap-2">
+            <span className="font-SemiBold text-lg">{label}</span>
+            <p className="font-RegularRoboto text-[#B6B6B6]">{desc}</p>
+          </div>
+        </CardContent>
+      </Card>
+    </motion.div>
   );
 }
--- src/pages/landing/GetStarted.jsx
+++ src/pages/landing/GetStarted.jsx
@@ -6,11 +6,20 @@ import { Button } from "@/components/ui/button";
 
 import { GetStartedInfo } from "@/constants";
 
+import { motion } from "motion/react";
 export default function GetStarted() {
   return (
-    <div className="mt-44 flex h-[80dvh] w-full items-center ~px-5/24">
+    <div className="mt-44 flex h-[80dvh] w-full items-center ~px-5/24 overflow-hidden">
       <div className="flex w-full flex-col items-start justify-between gap-10 md:flex-row">
-        <div className="space-y-6">
+        <motion.div
+          initial={{ x: -100, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{
+            duration: 1.5,
+            delay: 1,
+          }}
+          className="space-y-6"
+        >
           <span className="font-Bold ~text-2xl/4xl">How To Get Started</span>
           <p className="max-w-[23rem] font-RegularRoboto text-lg text-[#B6B6B6]">
             Simple and easy way to start your investment in cryptocurrency
@@ -18,10 +27,10 @@ export default function GetStarted() {
           <Button className="bg-[#0FAE96] px-10 font-SemiBold" size="lg">
             Get Started
           </Button>
-        </div>
+        </motion.div>
         <div className="space-y-6">
-          {GetStartedInfo.map((item) => (
-            <CardGetStarted key={item.id} {...item} />
+          {GetStartedInfo.map((item, index) => (
+            <CardGetStarted key={item.id} {...item} index={index} />
           ))}
         </div>
       </div>
